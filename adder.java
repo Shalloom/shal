42effc57800547b8972a60c8cbd78bbc35a15428
Shalloom <shal71124@gmail.com>
2024-11-12 08:46:50 -0800

    Add files via upload

---
+++ adder.java
@@ -0,0 +1,12 @@
+public  class adder{
+    public static int add(int a,int b){
+        return a+b;
+    }
+    public static int add(int a,int b,int c){
+        return a+b+c;
+    }
+    public static void main(String arg[]){
+    System.out.println(add(3,4));
+    System.out.println(add(3,4,7));
+    }
+    }
